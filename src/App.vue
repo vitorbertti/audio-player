68eb5c394de8955e2aef027ae6c058872191efeb
Vitor Bertti <berttivitor@gmail.com>
2020-09-27 20:56:17 -0300

    Added files

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+   <div id="app">
+      <div id="nav">
+         <router-link to="/">Home</router-link> |
+         <router-link to="/about">About</router-link>
+      </div>
+      <router-view />
+   </div>
 </template>
 
 <style lang="scss">
+@import 'https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css';
+@import 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+   a {
+      font-weight: bold;
+      color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+      &.router-link-exact-active {
+         color: #42b983;
+      }
+   }
 }
 </style>
