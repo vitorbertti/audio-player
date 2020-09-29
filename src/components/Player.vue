0d7c73ca46b60ef9aea52969b61c125158e0c604
Vitor Bertti <berttivitor@gmail.com>
2020-09-28 21:57:45 -0300

    Added Player component

---
+++ src/components/Player.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h4><i class="material-icons">equalizer</i>Podcast</h4>
+    <div class="card">
+      <div class="card-image"></div>
+      <div class="card-content"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Player',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
