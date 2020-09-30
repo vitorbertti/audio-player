a067349bb7056ba1e1fe48362e06e510216b11c0
Vitor Bertti <berttivitor@gmail.com>
2020-09-30 19:35:38 -0300

    Added audio player using js

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -6,7 +6,7 @@
       <div class="card-content">
         <h5>Title</h5>
         <p class="artist">Artist</p>
-        <audio controls src="../assets/files/como-comecei.mp3"></audio>
+        <audio controls ></audio>
       </div>
     </div>
   </div>
@@ -15,7 +15,24 @@
 <script>
 export default {
   name: 'Player',
-  
+  mounted() {
+    const cover = document.querySelector('.card-image');
+    const title = document.querySelector('.card-content h5');
+    const artist = document.querySelector('.card-content .artist');
+    const audio = document.querySelector('audio');
+
+    const data = {
+      title: 'Como começei a programar / Por que criamos a Rocketseat / Nossa Stack',
+      artist: 'Diego Fernandes',
+      cover: '../assets/files/como-comecei.jpg',
+      file: '../assets/files/como-comecei.mp3',
+    };
+
+    cover.style.backgrond = `url('${data.cover}') no-repeat center center / cover`;
+    title.innerText = data.title;
+    artist.innerText = data.artist;
+    audio.src = data.file;
+  },
 }
 </script>
 
