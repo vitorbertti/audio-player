b03c5037f49e6ac03d4cdcd7773ad317231ebb6b
Vitor Bertti <berttivitor@gmail.com>
2020-10-04 22:35:39 -0300

    Removed initial nav menu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
    <div id="app">
-      <div id="nav">
-         <router-link to="/">Home</router-link> |
-         <router-link to="/about">About</router-link>
-      </div>
       <router-view />
    </div>
 </template>
@@ -20,7 +16,7 @@ body {
    justify-content: center;
 }
 
-#player {
+.player {
    background-color: white;
    width: 90%;
    max-width: 375px;
--- src/components/Player.vue
+++ src/components/Player.vue
@@ -2,11 +2,12 @@
   <div class="player">
     <h4><i class="material-icons">equalizer</i>Podcast</h4>
     <div class="card">
-      <div class="card-image"></div>
+      <div class="card-image" :style="cover"></div>
       <div class="card-content">
         <h5>Title</h5>
         <p class="artist">Artist</p>
-        <audio controls ></audio>
+        <audio controls :src="audio" ></audio>
+        
       </div>
     </div>
   </div>
@@ -19,12 +20,12 @@ export default {
   name: 'Player',
   data() {
     return {
-      player: {
-        cover: document.querySelector('.card-image'),
-        title: document.querySelector('.card-content h5'),
-        artist: document.querySelector('.card-content .artist'),
-        audio: document.querySelector('audio'),
-      }
+      // player: {
+        cover: {},
+        title: {},
+        artist: {},
+        audio: {},
+      // }
     }
   },
   mounted() {
@@ -32,11 +33,13 @@ export default {
   },
   methods: {
     start() {
-        this.player.cover.style.backgrond = `url('${data.cover}') no-repeat center center / cover`;
-        this.player.title.innerText = data.title;
-        this.player.artist.innerText = data.artist;
-        this.player.audio.src = data.file;
-        this.player.audio.addEventListener('ended', () => {})
+        this.cover = { background: `url('${data[0].cover}') no-repeat center center / cover` };
+        this.title = document.querySelector('.card-content h5');
+        this.title.innerText = data[0].title;
+        this.artist = document.querySelector('.card-content .artist');
+        this.artist.innerText = data[0].artist;
+        this.audio = data[0].file;
+        // this.audio.addEventListener('ended', () => {})
       },
   }
 }
