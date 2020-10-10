5f4034525eabc5e78a5f772e55adec3677c41e95
Vitor Bertti <berttivitor@gmail.com>
2020-10-10 19:53:55 -0300

    Added actions and play functions

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,124 +1,128 @@
 <template>
-  <div class="player">
-    <h4><i class="material-icons">equalizer</i>Podcast</h4>
-    <div class="card">
-      <div class="card-image" :style="cover">
-        <a href="#" class="btn-floating btn-large halfway-fab">
-          <i class="material-icons" id="play-pause">play_arrow</i>
-        </a>
-      </div>
-      <div class="card-content">
-        <h5>Title</h5>
-        <p class="artist">Artist</p>
-
-        <div class="row valign-wrapper">
-          <div class="col s2" id="current-duration">00:00</div>
-          <div class="col s6 range-field valign-wrapper">
-            <input type="range" min="0" max="0" id="seekbar" step="1">
-          </div>
-
-
-          <div class="col s2" id="total-duration">00:00</div>
-          <div>
-            <a href="#">
-              <i class="material-icons" id="vol">volume_up</i>
+   <div class="player">
+      <h4><i class="material-icons">equalizer</i>Podcast</h4>
+      <div class="card">
+         <div class="card-image" :style="cover">
+            <a href="#" class="btn-floating btn-large halfway-fab">
+               <i class="material-icons" id="play-pause">play_arrow</i>
             </a>
-            
-          </div>
-          <div class="col s3 range-field valign-wrapper">
-              <input type="range" min="0" max="100" id="vol-control" step="1">
-          </div>
-          
-        </div>
-        
+         </div>
+         <div class="card-content">
+            <h5>Title</h5>
+            <p class="artist">Artist</p>
+
+            <div class="row valign-wrapper">
+               <div class="col s2" id="current-duration">00:00</div>
+               <div class="col s6 range-field valign-wrapper">
+                  <input type="range" min="0" max="0" id="seekbar" step="1" />
+               </div>
+
+               <div class="col s2" id="total-duration">00:00</div>
+               <div>
+                  <a href="#">
+                     <i class="material-icons" id="vol">volume_up</i>
+                  </a>
+               </div>
+               <div class="col s3 range-field valign-wrapper">
+                  <input type="range" min="0" max="100" id="vol-control" step="1" />
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
-  </div>
+   </div>
 </template>
 
 <script>
 import data from '../service/data';
 
 export default {
-  name: 'Player',
-  data() {
-    return {
-      // player: {
-        cover: {},
-        title: {},
-        artist: {},
-        audioData: {},
-        currentAudio: {},
-        currentPlaying: 0,
-        audio: {},
-      // }
-    }
-  },
-  mounted() {
-    window.addEventListener('load', this.start());
-  },
-  methods: {
-    start() {
-        this.cover = document.querySelector('.card-image');
-        this.title = document.querySelector('.card-content h5');
-        this.artist = document.querySelector('.card-content .artist');
-        // this.audio = document.querySelector('audio');
+   name: 'Player',
+   data() {
+      return {
+         // player: {
+         cover: {},
+         title: {},
+         artist: {},
+         audioData: {},
+         currentAudio: {},
+         currentPlaying: 0,
+         audio: {},
+         playPause: {},
+         // }
+      };
+   },
+   mounted() {
+      window.addEventListener('load', this.start());
+   },
+   methods: {
+      start() {
+         this.cover = document.querySelector('.card-image');
+         this.title = document.querySelector('.card-content h5');
+         this.artist = document.querySelector('.card-content .artist');
+         this.playPause = document.querySelector('#play-pause');
+         // this.audio = document.querySelector('audio');
 
-        this.update();
+         this.update();
 
-        this.audio.onended  = () => this.next();
-    },
-    next() {
-      this.currentPlaying++;
-      if(this.currentPlaying == this.audioData.length) {
-        this.restart();
-      }
-      this.update();
-    },
-    update() {
-      this.currentAudio = this.audioData[this.currentPlaying];
+         this.audio.onended = () => this.next();
+      },
+      next() {
+         this.currentPlaying++;
+         if (this.currentPlaying == this.audioData.length) {
+            this.restart();
+         }
+         this.update();
+      },
+      update() {
+         this.currentAudio = this.audioData[this.currentPlaying];
 
-      this.cover = { background: `url('${this.path(data[currentPlaying].cover)}') no-repeat center center / cover` };
-      this.title.innerText = data[currentPlaying].title;
-      this.artist.innerText = data[currentPlaying].artist;
-      this.createAudioElement(this.path(this.currentAudio.file));
-    },
-    createAudioElement(audio) {
-      this.audio = new Audio(audio);
-    },
-    restart() {
-      this.currentPlaying = 0;
-      this.update();
-    },
-    path(file) {
-      return `../assets/files/${file}`;
-    },
-
-  }
-}
+         this.cover = {
+            background: `url('${this.path(data[currentPlaying].cover)}') no-repeat center center / cover`,
+         };
+         this.title.innerText = data[currentPlaying].title;
+         this.artist.innerText = data[currentPlaying].artist;
+         this.createAudioElement(this.path(this.currentAudio.file));
+      },
+      createAudioElement(audio) {
+         this.audio = new Audio(audio);
+      },
+      actions() {
+        
+        this.
+      },
+      play() {
+        this.audio.play();
+      },
+      restart() {
+         this.currentPlaying = 0;
+         this.update();
+      },
+      path(file) {
+         return `../assets/files/${file}`;
+      },
+   },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+#play-pause {
+   background-color: #7159c1;
+}
 
-  #play-pause {
-    background-color: #7159c1;
-  }
+#vol {
+   color: #7159c1;
+}
 
-  #vol {
-    color: #7159c1;
-  }
+input[type='range'] ::-webkit-slider-thumb {
+   background-color: #7159c1;
+}
 
-  input[type="range"] ::-webkit-slider-thumb {
-    background-color: #7159c1;
-  }
+input[type='range'] ::-moz-range-thumb {
+   background-color: #7159c1;
+}
 
-  input[type="range"] ::-moz-range-thumb {
-    background-color: #7159c1;
-  }
-
-  input[type="range"] ::-ms-thumb {
-    background-color: #7159c1;
-  }
-
+input[type='range'] ::-ms-thumb {
+   background-color: #7159c1;
+}
 </style>
